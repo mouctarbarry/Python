543384946155029f306bf474e58f08e0889cdbbe
mouctabarry <bmouctar22@gmail.com>
2023-08-05 22:34:30 +0200

    person

--- School's/decrypt.py
+++ School's/decrypt.py
@@ -2,7 +2,7 @@ f4g = {}  # dic des fréquences des 4-grammes
 f = open('brut4g_fr.txt')
 total = 0  # effectif total
 for line in f:
-    (w, c) = line.split(sep=' ')
+    (w, c) = line.split()
     f4g[w] = int(c)
     total += int(c)
     print(total)
@@ -11,3 +11,24 @@ for w in f4g:
     # print (f4g)
 f.close()
 print(f4g['MENT'])
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def named(self):
+        print(f"Hello {self.name}")  # Use self.name to access the instance attribute
+
+    def talk(self):
+        print(f"{self.name} speaks English")  # Use self.name to access the instance attribute
+
+
+# Creating an instance of the Person class
+person1 = Person("John")
+
+# Calling the named method to greet the person
+person1.named()
+
+# Calling the talk method to describe the language the person speaks
+person1.talk()
