3d1f4cb9934b0b032bc857cd4050fc52e33d661a
mouctabarry <bmouctar22@gmail.com>
2023-08-05 22:39:33 +0200

    person and decrypt

--- School's/decrypt.py
+++ School's/decrypt.py
@@ -1,34 +1,34 @@
-f4g = {}  # dic des fréquences des 4-grammes
-f = open('brut4g_fr.txt')
-total = 0  # effectif total
-for line in f:
-    (w, c) = line.split()
-    f4g[w] = int(c)
-    total += int(c)
-    print(total)
-for w in f4g:
-    f4g[w] /= total  # calcul des fréquences
-    # print (f4g)
-f.close()
-print(f4g['MENT'])
+f4g = {}  # Dictionary to store frequencies of 4-grams
+total = 0  # Total count of 4-grams
+
+# Read data from the file and calculate frequencies
+try:
+    with open('brut4g_fr.txt') as f:
+        for line in f:
+            w, c = line.split()
+            f4g[w] = int(c)
+            total += int(c)
+
+    # Calculate the frequencies
+    for w in f4g:
+        f4g[w] /= total
+
+    # Print the frequency of 'MENT'
+    print(f4g['MENT'])
+
+except FileNotFoundError:
+    print("File not found.")
+except ValueError:
+    print("Incorrect file format. Ensure each line has a 4-gram and its count separated by space.")
 
 
 class Person:
     def __init__(self, name):
         self.name = name
 
-    def named(self):
-        print(f"Hello {self.name}")  # Use self.name to access the instance attribute
-
     def talk(self):
-        print(f"{self.name} speaks English")  # Use self.name to access the instance attribute
+        print(f"Hi, I'm {self.name}")  # Use self.name to access the instance attribute
 
 
-# Creating an instance of the Person class
 person1 = Person("John")
-
-# Calling the named method to greet the person
-person1.named()
-
-# Calling the talk method to describe the language the person speaks
 person1.talk()
