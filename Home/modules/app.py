060429799954687bf2d1609e33882d9e04bab155
mouctabarry <bmouctar22@gmail.com>
2023-08-06 01:42:38 +0200

    automation first commit

--- Home/modules/app.py
+++ Home/modules/app.py
@@ -1,12 +1,4 @@
-import random
+from pathlib import Path
 
-
-class Dice:
-    def roll(self):
-        first = random.randint(1, 6)
-        second = random.randint(1, 6)
-        return first, second
-
-
-dice = Dice()
-print(dice.roll())
+path = Path("../ecommerce")
+print(path.exists())
