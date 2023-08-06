34a6fd306d2b9931080a331f30540037493b8317
mouctabarry <bmouctar22@gmail.com>
2023-08-06 02:00:01 +0200

    correcting prices

--- Home/automation/app.py
+++ Home/automation/app.py
@@ -6,4 +6,8 @@ cell = sheet['a1']
 
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    print(cell.value)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transaction2.xlsx')
