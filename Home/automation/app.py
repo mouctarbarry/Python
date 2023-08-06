4c062b9d07cdfe7c57690c8b938b5726373886fc
mouctabarry <bmouctar22@gmail.com>
2023-08-06 11:19:30 +0200

    func change price done

--- Home/automation/app.py
+++ Home/automation/app.py
@@ -1,24 +1,25 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-cell = sheet['a1']
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-values = Reference(sheet,
-                   min_row=2,
-                   max_row=sheet.max_row,
-                   min_col=4,
-                   max_col=4)
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
 
-wb.save('transaction2.xlsx')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
